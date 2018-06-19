b5b1f312a82116b46df0876a55ca2a3d8c038254
mlandreau <manuel.landreau@netapsys.fr>
2018-06-19 14:58:46 +0200

    ok

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from 'mqtt';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
