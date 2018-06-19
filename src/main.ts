504ac55d4a8de447de1b1b60719e2414b28107ff
mlandreau <manuel.landreau@netapsys.fr>
2018-06-19 14:21:29 +0200

    mise en place mqtt

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from 'mqtt';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // https://docs.nestjs.com/microservices/basics
+  // Décommenter quand le server MQTT sera disponible
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   transport: Transport.MQTT,
+  //   options: {
+  //     host: 'localhost',
+  //     port: 1883,
+  //   },
+  // });
+
   await app.listen(3000);
 }
+
 bootstrap();
